67dd1bfddf7368e65b43aeadd925c759b79883f1
rahul.gorad <rahul.gorad@cmssl.in>
2024-06-15 14:03:12 +0530

    build: working

--- src/main/java/com/kkh_chth/articles/batch/SpringBatch/config/JobCompletionStatusImpl.java
+++ src/main/java/com/kkh_chth/articles/batch/SpringBatch/config/JobCompletionStatusImpl.java
@@ -18,7 +18,7 @@ public class JobCompletionStatusImpl implements JobExecutionListener {
 
     @Override
     public void afterJob(JobExecution jobExecution) {
-        if(jobExecution.getStatus()== BatchStatus.COMPLETED){
+        if(jobExecution.getStatus() == BatchStatus.COMPLETED){
             log.info("Job Completed");
         }
     }
